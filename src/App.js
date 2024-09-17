52b1dde8b446cc5cb1c782f477cc5c60bb5edcff
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-09-17 19:31:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,29 @@ function App() {
   return (
     <div className="App">
       <Card>
-          <h2>Card Title</h2>
-          <p>Card Description</p>
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+        <p><strong>Asignado a:</strong>Juan</p>
+        <p><strong>Fecha de Inicio:</strong> 12/12/2021</p>
+        <p><strong>Fecha de Fin:</strong> 12/12/2021</p>
       </Card>
+
+      <Card>
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+        <p><strong>Asignado a:</strong>Juan</p>
+        <p><strong>Fecha de Inicio:</strong> 12/12/2021</p>
+        <p><strong>Fecha de Fin:</strong> 12/12/2021</p>
+      </Card>
+
+      <Card>
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+        <p><strong>Asignado a:</strong>Juan</p>
+        <p><strong>Fecha de Inicio:</strong> 12/12/2021</p>
+        <p><strong>Fecha de Fin:</strong> 12/12/2021</p>
+      </Card>
+      
       <InputComponent />
     </div>
   );
